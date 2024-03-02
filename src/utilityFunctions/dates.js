cd410f9c90fbd3ac3fa093d24d7e7e98a8e93054
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-03-02 19:48:34 +0100

    US4. Fixed type date
    Fixede type date to ISOString everywhere so there is no format problem with backend

--- src/utilityFunctions/dates.js
+++ src/utilityFunctions/dates.js
@@ -1,3 +1,20 @@
+export const createDateFromTimeString = (date,timeString) =>{
+    // Séparer l'heure et les minutes en utilisant la méthode split()
+    const [heureStr, minutesStr] = timeString.split('h');
+
+    // Convertir les chaînes de caractères en nombres
+    const heureNum = parseInt(heureStr, 10);
+    const minutesNum = parseInt(minutesStr, 10);
+
+    // Créer une nouvelle instance de Date avec l'heure et les minutes spécifiées
+    date.setHours(heureNum+1);
+    date.setMinutes(minutesNum);
+
+    const ISOStringDate = new Date(date).toISOString()
+    
+    return ISOStringDate;
+}
+
 export const convertDecimalToTime= (decimalTime) =>{
     // Séparer la partie entière et décimale
     const hours = Math.floor(decimalTime);
@@ -24,18 +41,39 @@ export const convertToStartOfDay = (date) =>{
 
 }
 
-export const createDateFromTimeString = (date,timeString) =>{
-    // Séparer l'heure et les minutes en utilisant la méthode split()
-    const [heureStr, minutesStr] = timeString.split('h');
+// Convert hours from date type to xxhxx   (hours and minutes)
+export const  formatTimeText = (dateProps) =>{
+    const date = new Date(dateProps)
+    // Récupérer les heures et les minutes de la date
+    const hours = date.getHours()-1;
+    const minutes = date.getMinutes();
 
-    // Convertir les chaînes de caractères en nombres
-    const heureNum = parseInt(heureStr, 10);
-    const minutesNum = parseInt(minutesStr, 10);
+    // Ajouter un zéro devant les minutes si elles sont inférieures à 10
+    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
 
-    // Créer une nouvelle instance de Date avec l'heure et les minutes spécifiées
-    date.setHours(heureNum);
-    date.setMinutes(minutesNum);
+    // Récupérer le reste de la date
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Les mois sont indexés à partir de 0, donc ajouter 1
+    const day = date.getDate();
+
+    // Formater la date en format "YYYY-MM-DD"
+    const dateFormatted = `${day < 10 ? `0${day}` : day}/${month < 10 ? `0${month}` : month}/${year}`;
+
+    // Retourner l'heure et le reste de la date formatés
+    return {date:dateFormatted, hour:`${hours}h${minutesFormatted}`};
+}
+
+
+export const resetSecondsAndMs = (date) =>{
+    if (!(date instanceof Date)) {
+        throw new Error('La valeur passée en paramètre n\'est pas une instance de Date.');
+    }
+
+    date.setSeconds(0);
+    date.setMilliseconds(0);
 
     return date;
 }
+
+
 

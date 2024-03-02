9da1f5a18ac426add21192b96f576784d56dff1b
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-03-02 20:02:25 +0100

    Added react-router
    Added react-router
    Added appointment route
    Added homePage route
    Added background image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import Appointment from './screens/appointment/appointment'
+import HomePage from "./screens/homePage/homePage";
+import AppointmentConfirmation from "./components/appointmentConfirmation/appointmentConfirmation";
 
 
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Appointment/>
-
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<HomePage/>}/>
+          <Route path="appointment" element={<Appointment/>}/>
+          <Route path="appointmentConfirmation/" element = {<AppointmentConfirmation/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
